f60939058ec791b247f27ef3382d3cb32a123e48
Dev Emmy <emmanuelonyeabor68@gmail.com>
2026-03-28 13:04:53 -0700

    updated

--- config/config/settings.py
+++ config/config/settings.py
@@ -11,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dh#c3bn-k@rw2w3p-e*9h7xb$$7y76fvb+-nc79&la3=#j(ru4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'FALSE') == 'TRUE'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -67,11 +67,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-        
+    'default': {
+        "ENGINE": "django.db.backends.sqlite",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+
+ }
+
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
+
+
 
 
 
