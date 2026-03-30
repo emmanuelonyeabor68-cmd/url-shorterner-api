10f825871abcb0e57365ed206c7b8201de5aa3fc
Dev Emmy <emmanuelonyeabor68@gmail.com>
2026-03-29 23:52:37 -0700

    updated routing

--- config/config/urls.py
+++ config/config/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
     path('api/', include("shortener.urls")),
-    path('<str:short_code>/', include("shortener.urls_redirect_url")),
+    path('', include("shortener.urls")),
 ]
